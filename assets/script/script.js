2590193032e8afe86aea6c22a362bc9e6142b7ab
Kailen James <sevenflags57@gmail.com>
2023-01-06 14:12:19 -0800

    Wrote code to initialize the page with colored blocks and set the day at the top

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,23 +1,41 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+const BLOCKS = [];
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+	const TIME_BLOCK_CONTAINER = document.querySelector("#time-block-container");
+	const TIME_BLOCK_TEMPLATE = document.querySelector("#time-block-template");
+	const CURRENT_DATE = document.querySelector("#current-day");
+
+	const HOUR_LABELS = ["12AM", "1AM", "2AM", "3AM", "4AM", "5AM", "6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM", "11PM"];
+
+	//const getTimeData = time
+
+	const addTimeBlock = (time, cssClass) => {
+		const frag = TIME_BLOCK_TEMPLATE.content.cloneNode(true);
+		const root = frag.querySelector("#hour-");
+		root.setAttribute ("id", `hour-${time}`);
+		root.querySelector("div").innerHTML = HOUR_LABELS[time];
+		const textArea = root.querySelector("textarea");
+		root.classList.add(cssClass);
+		root.querySelector("button").addEventListener("click", function() {
+			// Save button pressed
+			console.log(textArea.value);
+		});
+		TIME_BLOCK_CONTAINER.appendChild(frag);
+		BLOCKS.push(root);
+	};
+
+	const hour = dayjs().hour();
+	for(let i = 0; i < 24; ++i)
+	{
+		let cssClass = "past";
+		if(hour == i) {cssClass = "present";}
+		else if( i > hour) {cssClass = "future";}
+		
+		addTimeBlock(i, cssClass)
+	}
+
+	CURRENT_DATE.textContent = dayjs().format("MMMM DD, YYYY");
 });
